3bac56aa0c23404e56ae75b8d4f150aba16f2c94
Gokana Emmanuel <emmanuelgokana@gmail.com>
2023-09-14 13:37:14 +0200

    Incorporation page jofre

--- lifprojet2.py
+++ lifprojet2.py
@@ -5,9 +5,11 @@ import plotly.express as px
 from lifprojet import *
 app = Dash(__name__)
 
-layout = html.Div([
+app.layout = html.Div([
     html.H1("GRAPHIQUE"),
     dcc.Graph(figure=px.histogram(df, x='Sex', y='Age', histfunc='avg'),
-              style = {'width' : 500})
+              style = {'width' : 500}, id="my-graph")
     # Autres composants pour la page 1
 ])
+
+layout2 = app.layout
