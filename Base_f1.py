7fa6e7db5396b959599b0d746b864a485f86d5b5
Emmanuel Gokana <emmanuelgokana@gmail.com>
2023-09-21 19:55:42 +0200

    merge jofrix

--- Base_f1.py
+++ Base_f1.py
@@ -138,11 +138,4 @@ def page1_layout():
                     style = {"font-family" : "verdana"}),
             style = { "background-color" : "antiquewhite"}),
         dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-
-        html.Div(
-            html.H1("Tableau",
-                    style = {"font-family" : "verdana"}),
-            style = { "background-color" : "antiquewhite"}),
-        dash_table.DataTable(data=merged_df.to_dict('records'), page_size=10),
-
         ])
