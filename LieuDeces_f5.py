38ba36bb1d1079e63be161b01301080f723f5044
Emmanuel Gokana <emmanuelgokana@gmail.com>
2023-09-29 17:07:40 +0200

    finition de la carte deces

--- LieuDeces_f5.py
+++ LieuDeces_f5.py
@@ -9,10 +9,10 @@ toulouse = df[df['Death Place'].str.contains('TOULOUSE', case=False, regex=True)
 lyon = df[df['Death Place'].str.contains('LYON', case=False, regex=True)]
 marseille = df[df['Death Place'].str.contains('MARSEILLE', case=False, regex=True)]
 
-nb_mort_paris = int(paris.size)
-nb_mort_toulouse = int(toulouse.size)
-nb_mort_lyon = int(lyon.size)
-nb_mort_marseille = int(marseille.size)
+nb_mort_paris = len(paris)
+nb_mort_toulouse = len(toulouse)
+nb_mort_lyon = len(lyon)
+nb_mort_marseille = len(marseille)
 
 colors = {
     'background': '#FFFFFF',
@@ -21,19 +21,19 @@ colors = {
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
+dff = pd.DataFrame({
     "Ville": ["Paris","Lyon","Toulouse","Marseille"],
-    "Amount": [nb_mort_paris, nb_mort_lyon, nb_mort_toulouse, nb_mort_marseille]
+    "Nombre": [nb_mort_paris, nb_mort_lyon, nb_mort_toulouse, nb_mort_marseille]
 })
 
-fig = px.bar(df, x="Ville", y="Amount")
+fig = px.bar(dff, x="Ville", y="Nombre")
 
 
 def page5_layout():
         return html.Div(style={'backgroundColor': colors['background']}, children=[
             dcc.Graph(id='example-graph-2', figure=fig),
             dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': 'Ville', 'id': 'Ville'}, {'name': 'Nb Morts', 'id': 'Amount'}],
+            data=dff.to_dict('records'),
+            columns=[{'name': 'Ville', 'id': 'Ville'}, {'name': 'Nb Morts', 'id': 'Nombre'}],
         ),
 ])
